7aec17229f9429344592c85b26b721717e57c68c
Emanuel Machado da Silva <emanuel.mch@gmail.com>
2012-12-17 22:10:33 -0200

    Initial commit

---
+++ BaTTTlestar.Model/Game.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BaTTTlestar.Model
+{
+    public class Game
+    {
+        public Board Board { get; set; }
+        public IPlayer Player1 { get; set; }
+        public IPlayer Player2 { get; set; }
+    }
+}
