42bd9d0d025a9e23b4447f980d9c1afcec5e43dd
Emanuel Machado da Silva <emanuel.mch@gmail.com>
2012-12-19 20:48:07 -0200

    Start ConsoleGame implementation

---
+++ BaTTTlestar.Shell.Console/ConsoleGame.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using BaTTTlestar.Model;
+
+namespace BaTTTlestar.Shell.Console
+{
+    public class ConsoleGame
+    {
+        private Game _game;
+
+        public ConsoleGame(Game game)
+        {
+            this._game = game;
+        }
+
+        public void Run()
+        {
+            System.Console.WriteLine("BaTTTlestar by Bill");
+            System.Console.ReadKey();
+        }
+    }
+}
--- BaTTTlestar.Shell.Console/Program.cs
+++ BaTTTlestar.Shell.Console/Program.cs
@@ -4,12 +4,20 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using BaTTTlestar.Model;
+
 namespace BaTTTlestar.Shell.Console
 {
     public class Program
     {
         public static void Main(string[] args)
         {
+            Game game = new Game();
+            game.Player1 = null;
+            game.Player2 = null;
+
+            ConsoleGame shell = new ConsoleGame(game);
+            shell.Run();
         }
     }
 }
