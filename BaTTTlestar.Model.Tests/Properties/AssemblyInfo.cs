3e300e74c62ff68e7bee12b104fbe8530e69b290
Emanuel Machado da Silva <emanuel.mch@gmail.com>
2012-12-23 19:24:11 -0200

    Recreate Model.Tests project

--- BaTTTlestar.Model.Tests/Properties/AssemblyInfo.cs
+++ BaTTTlestar.Model.Tests/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("9bda8f75-1940-4cfe-91c5-ae00e9eff826")]
+[assembly: Guid("771e9dec-feaf-40be-a239-786cd38f89b3")]
 
 // Version information for an assembly consists of the following four values:
 //
