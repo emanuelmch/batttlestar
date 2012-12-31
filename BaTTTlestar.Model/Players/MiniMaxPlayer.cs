022da37c2986cda361b2e5cbf2013d7a665c18ef
Emanuel Machado da Silva <emanuel.mch@gmail.com>
2012-12-30 22:21:41 -0200

    Create a new player: MiniMaxPlayer

---
+++ BaTTTlestar.Model/Players/MiniMaxPlayer.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+using BaTTTlestar.Model.Data;
+
+namespace BaTTTlestar.Model.Players
+{
+    public class MiniMaxPlayer : IPlayer
+    {
+        public string Name { get; set; }
+        public Board Board { get; set; }
+        public int Value { get; set; }
+
+        public MiniMaxPlayer(string name)
+        {
+            this.Name = name;
+        }
+
+        public Move Move()
+        {
+            return null;
+        }
+
+    }
+}
