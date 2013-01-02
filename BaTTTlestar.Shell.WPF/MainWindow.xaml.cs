ee2556ff676042a1176d368665143f2a3362bbc7
Emanuel Machado da Silva <emanuel.mch@gmail.com>
2013-01-01 22:04:21 -0200

    Create new project: Shell.WPF

---
+++ BaTTTlestar.Shell.WPF/MainWindow.xaml.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Windows;
+
+namespace BaTTTlestar.Shell.WPF
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
